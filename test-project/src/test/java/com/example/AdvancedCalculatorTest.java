d5cdc511dccce2eca8f9590338dcd482c7d95ff4
Steven Van Ingelgem <steven@vaningelgem.be>
2025-02-22 10:47:58 +0100

    Disable tests to reduce the coverage

--- test-project/src/test/java/com/example/AdvancedCalculatorTest.java
+++ test-project/src/test/java/com/example/AdvancedCalculatorTest.java
@@ -14,14 +14,16 @@ class AdvancedCalculatorTest {
     void testDivide() {
         AdvancedCalculator advCalc = new AdvancedCalculator();
         assertEquals(2, advCalc.divide(4, 2));
-        assertThrows(IllegalArgumentException.class, () -> advCalc.divide(1, 0));
+        // Disabling this branch
+        // assertThrows(IllegalArgumentException.class, () -> advCalc.divide(1, 0));
     }
 
     @Test
     void testCategorizeNumber() {
         AdvancedCalculator advCalc = new AdvancedCalculator();
         assertEquals("Positive", advCalc.categorizeNumber(5));
-        assertEquals("Negative", advCalc.categorizeNumber(-3));
+        // Disabling this branch
+        // assertEquals("Negative", advCalc.categorizeNumber(-3));
         assertEquals("Zero", advCalc.categorizeNumber(0));
     }
 }
--- test-project/src/test/java/com/example/CalculatorTest.java
+++ test-project/src/test/java/com/example/CalculatorTest.java
@@ -10,9 +10,12 @@ class CalculatorTest {
         assertEquals(5, calc.add(2, 3));
     }
 
+    /*
+    // Disabling test in order to have 1 method not covered
     @Test
     void testSubtract() {
         Calculator calc = new Calculator();
         assertEquals(1, calc.subtract(3, 2));
     }
+    */
 }
